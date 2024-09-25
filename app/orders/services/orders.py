9edfdce39a80c479b4b283f6481bbaa6a5a1cf97
Anton Korol <korol142@gmail.com>
2024-09-25 16:06:29 +0300

    add dockstrings

--- app/orders/services/orders.py
+++ app/orders/services/orders.py
@@ -6,6 +6,13 @@ from database.models import Order, Product, OrderItem, Status
 
 
 def create_new_order(order: OrderCreateSchema, session: Session) -> Order:
+    """
+    Создание нового заказа.
+
+    :param order: Схема заказа, содержащая информацию о товаре и количестве.
+    :param session: Сессия для взаимодействия с базой данных.
+    :return: Созданный объект заказа.
+    """
     order_items = create_order_items_list(order, session)
     new_order = Order()
     session.add(new_order)
@@ -15,6 +22,13 @@ def create_new_order(order: OrderCreateSchema, session: Session) -> Order:
     return new_order
 
 def create_order_items_list(order: OrderCreateSchema, session: Session) -> list:
+    """
+    Создание списка элементов заказа.
+
+    :param order: Схема заказа, содержащая информацию о товаре и количестве.
+    :param session: Сессия для взаимодействия с базой данных.
+    :return: Список с элементами заказа
+    """
     order_items = []
     for item in order.order_items:
         product_id = item.product_id
@@ -33,11 +47,24 @@ def create_order_items_list(order: OrderCreateSchema, session: Session) -> list:
     return order_items
 
 def stock_check(product: Product, quantity: int) -> None:
+    """
+    Проверка наличия товара на складе и обновление его количества.
+
+    :param product: Объект товара для проверки.
+    :param quantity: Количество товара для заказа.
+    """
     if product.stock < quantity:
         raise HTTPException(status_code=400, detail=f'There are not enough {product.name} in stock.')
     product.stock -= quantity
 
 def add_items_in_order(items: list[OrderItem], order: Order, session: Session) -> None:
+    """
+    Добавление элементов заказа в заказ и обновление суммы заказа.
+
+    :param items: Список элементов заказа.
+    :param order: Объект заказа.
+    :param session: Сессия для взаимодействия с базой данных.
+    """
     price = 0
     for item in items:
         item.order_id = order.id
@@ -48,16 +75,37 @@ def add_items_in_order(items: list[OrderItem], order: Order, session: Session) -
     session.refresh(order)
 
 def get_orders_list(session: Session) -> list[Order]:
+    """
+    Получение списка всех заказов из базы данных.
+
+    :param session: Сессия для взаимодействия с базой данных.
+    :return: Список объектов всех заказов.
+    """
     orders = session.query(Order).all()
     return orders
 
 def get_order(order_id: int, session: Session) -> Order:
+    """
+    Получение заказа по идентификатору.
+
+    :param order_id: Идентификатор заказа.
+    :param session: Сессия для взаимодействия с базой данных.
+    :return: Объект заказа.
+    """
     order = session.query(Order).filter(Order.id == order_id).first()
     if order is None:
         raise HTTPException(status_code=404, detail="Invalid order id")
     return order
 
 def update_status(order: Order, status_id: int, session: Session) -> Order:
+    """
+    Обновление статуса заказа.
+
+    :param order: Объект заказа для обновления статуса.
+    :param status_id: Идентификатор статуса.
+    :param session: Сессия, для взаимодействия с базой данных.
+    :return: Объект заказа.
+    """
     status = session.query(Status).filter(Status.id == status_id).first()
     if status is None:
         raise HTTPException(status_code=404, detail="Invalid status")
--- app/products/services/products.py
+++ app/products/services/products.py
@@ -6,6 +6,13 @@ from database.models import Product
 
 
 def create_new_product(product: ProductCreateSchema, session: Session ) -> Product:
+    """
+    Создание нового товара.
+
+    :param product: Схема товара, содержащая необходимую информацию.
+    :param session: Сессия, для взаимодействия с базой данных.
+    :return: Объект созданного товара.
+    """
     product_model = Product(**product.model_dump())
     session.add(product_model)
     session.commit()
@@ -13,16 +20,37 @@ def create_new_product(product: ProductCreateSchema, session: Session ) -> Produ
     return product_model
 
 def get_products_list(session: Session) -> list[Product]:
+    """
+    Получение списка всех товаров из базы данных.
+
+    :param session: Сессия, для взаимодействия с базой данных.
+    :return: Список объектов товаров.
+    """
     products = session.query(Product).all()
     return products
 
 def get_product_by_id(product_id: int, session: Session) -> Product:
+    """
+    Получение товара по идентификатору.
+
+    :param product_id: Идентификатор товара.
+    :param session: Сессия, для взаимодействия с базой данных.
+    :return: Объект товара.
+    """
     product = session.query(Product).filter(Product.id == product_id).first()
     if product is None:
         raise HTTPException(status_code=404, detail="Invalid product id")
     return product
 
 def edit_product(product: Product, new_product_info: ProductEditSchema, session: Session) -> Product:
+    """
+    Изменение данных о товаре.
+
+    :param product: Объект товара для изменения.
+    :param new_product_info: Схема товара, содержащая необходимую информацию.
+    :param session: Сессия, для взаимодействия с базой данных.
+    :return: Объект товара.
+    """
     product.name = new_product_info.name
     product.desc = new_product_info.desc
     product.price = new_product_info.price
@@ -33,5 +61,11 @@ def edit_product(product: Product, new_product_info: ProductEditSchema, session:
 
 
 def delete_product_by_id(product: Product, session: Session):
+    """
+    Удаление товара из базы данных.
+
+    :param product: Объект товара для удаления.
+    :param session: Сессия, для взаимодействия с базой данных.
+    """
     session.delete(product)
     session.commit()
